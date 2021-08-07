6d5e3cbd7f62228d9069309aab2b166f576415f4
Jonathan Leow <m-2186973@moe-dl.edu.my>
2021-08-07 16:05:54 +0800

    function

--- yt_downloader.py
+++ yt_downloader.py
@@ -2,6 +2,29 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 from pytube import YouTube
 
+def widget():
+    input_label = tk.Label(text = "URL: ")
+    input_label.grid(row = 0, column = 0, sticky = 'w', padx = 10) 
+
+    download_button = tk.Button(text = 'Download', command = download_video,)
+    download_button.grid(row = 3, column = 1, padx = 10)
+
+    input_url = tk.Entry(width = 55)
+    input_url.grid(row = 0, column = 1, sticky = 'e', padx = 10)
+
+    destination_label = tk.Label(text = 'Destination: ')
+    destination_label.grid(row = 1, column = 0, sticky = 'w', padx = 10)
+
+    input_destination = tk.Entry(width = 45, textvariable = download_path)
+    input_destination.grid(row = 1, column = 1, sticky = 'w', padx = 10)
+
+    destination_button = tk.Button(text = 'Browse', command = browse)
+    destination_button.grid(row = 1, column = 1, sticky = 'e', padx = 10)
+
+    again = tk.Button(text = 'Click to download new video', command=redo)
+    again.grid(row = 3, column = 1)
+    again.grid_remove()
+
 def redo():
     again.grid_remove()
     input_url.delete(0, 'end')
@@ -20,31 +43,16 @@ def browse():
     download_Directory = filedialog.askdirectory(initialdir="YOUR DIRECTORY PATH")
     download_path.set(download_Directory)
 
-window = tk.Tk()
-window.title('YT Video Downloader')
 
-download_path = tk.StringVar()
+if __name__ == '__main__':
+    window = tk.Tk()
+    window.title('YT Video Downloader')
 
-input_label = tk.Label(text = "URL: ")
-input_label.grid(row = 0, column = 0, sticky = 'w', padx = 10) 
+    again = None
+    input_url = 
+    download_button = None
+    download_path = tk.StringVar()
 
-download_button = tk.Button(text = 'Download', command = download_video,)
-download_button.grid(row = 3, column = 1, padx = 10)
+    widget()
 
-input_url = tk.Entry(width = 55)
-input_url.grid(row = 0, column = 1, sticky = 'e', padx = 10)
-
-destination_label = tk.Label(text = 'Destination: ')
-destination_label.grid(row = 1, column = 0, sticky = 'w', padx = 10)
-
-input_destination = tk.Entry(width = 45, textvariable = download_path)
-input_destination.grid(row = 1, column = 1, sticky = 'w', padx = 10)
-
-destination_button = tk.Button(text = 'Browse', command = browse)
-destination_button.grid(row = 1, column = 1, sticky = 'e', padx = 10)
-
-again = tk.Button(text = 'Click to download new video', command=redo)
-again.grid(row = 3, column = 1)
-again.grid_remove()
-
-window.mainloop()
+    window.mainloop()
