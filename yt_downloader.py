8bde30f878628a333861764902bfb17ffadaabba
Jonathan Leow <m-2186973@moe-dl.edu.my>
2021-08-07 17:08:59 +0800

    upgraded to using oop

--- yt_downloader.py
+++ yt_downloader.py
@@ -2,57 +2,58 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 from pytube import YouTube
 
-def widget():
-    input_label = tk.Label(text = "URL: ")
-    input_label.grid(row = 0, column = 0, sticky = 'w', padx = 10) 
+class YTdownloader():
 
-    download_button = tk.Button(text = 'Download', command = download_video,)
-    download_button.grid(row = 3, column = 1, padx = 10)
+    def __init__(self):
+        self.window = tk.Tk()
+        self.widgets()
 
-    input_url = tk.Entry(width = 55)
-    input_url.grid(row = 0, column = 1, sticky = 'e', padx = 10)
+    def widgets(self):
+        input_label = tk.Label(text = "URL: ")
+        input_label.grid(row = 0, column = 0, sticky = 'w', padx = 10) 
 
-    destination_label = tk.Label(text = 'Destination: ')
-    destination_label.grid(row = 1, column = 0, sticky = 'w', padx = 10)
+        self.download_button = tk.Button(text = 'Download', command = self.download_video)
+        self.download_button.grid(row = 2, column = 1, padx = 10)
 
-    input_destination = tk.Entry(width = 45, textvariable = download_path)
-    input_destination.grid(row = 1, column = 1, sticky = 'w', padx = 10)
+        self.input_url = tk.Entry(width = 55)
+        self.input_url.grid(row = 0, column = 1, sticky = 'e', padx = 10)
 
-    destination_button = tk.Button(text = 'Browse', command = browse)
-    destination_button.grid(row = 1, column = 1, sticky = 'e', padx = 10)
+        destination_label = tk.Label(text = 'Destination: ')
+        destination_label.grid(row = 1, column = 0, sticky = 'w', padx = 10)
 
-    again = tk.Button(text = 'Click to download new video', command=redo)
-    again.grid(row = 3, column = 1)
-    again.grid_remove()
+        self.download_path = tk.StringVar()
+        input_destination = tk.Entry(width = 45, textvariable = self.download_path)
+        input_destination.grid(row = 1, column = 1, sticky = 'w', padx = 10)
 
-def redo():
-    again.grid_remove()
-    input_url.delete(0, 'end')
-    download_button.grid()
+        destination_button = tk.Button(text = 'Browse', command = self.browse)
+        destination_button.grid(row = 1, column = 1, sticky = 'e', padx = 10)
 
-def download_video():
-    url = input_url.get()
-    download_folder = download_path.get()
-    yt = YouTube(url).streams.get_highest_resolution()
-    yt.download(download_folder)
-    download_button.grid_remove()
-    again.grid()
-    messagebox.showinfo("Notice", "Download is Complete")
+        self.again = tk.Button(text = 'Click to download new video', command=self.redo)
+        self.again.grid(row = 2, column = 1)
+        self.again.grid_remove()
 
-def browse():
-    download_Directory = filedialog.askdirectory(initialdir="YOUR DIRECTORY PATH")
-    download_path.set(download_Directory)
+    def download_video(self):
+        url = self.input_url.get()
+        download_folder = self.download_path.get()
+        yt = YouTube(url).streams.get_highest_resolution()
+        yt.download(download_folder)
+        self.download_button.grid_remove()
+        self.again.grid()
+        messagebox.showinfo("Notice", "Video has been downloaded in PATH:\n" + download_folder)
 
+    def browse(self):
+        download_Directory = filedialog.askdirectory()
+        self.download_path.set(download_Directory)
+
+    def redo(self):
+        self.again.grid_remove()
+        self.input_url.delete(0, 'end')
+        self.download_button.grid()
+
+def main():
+    program = YTdownloader()
+    program.window.title('YT Video Downloader')
+    program.window.mainloop()
 
 if __name__ == '__main__':
-    window = tk.Tk()
-    window.title('YT Video Downloader')
-
-    again = None
-    input_url = 
-    download_button = None
-    download_path = tk.StringVar()
-
-    widget()
-
-    window.mainloop()
+    main()
